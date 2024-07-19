6b05af3adc50aba1a77489479b5ff59cc38d790c
Sundar M  Das <34454614+sundarmd@users.noreply.github.com>
2024-07-19 09:01:17 +0200

    Update real_estate_snowpipe.sql

--- real_estate_snowpipe.sql
+++ real_estate_snowpipe.sql
@@ -49,7 +49,7 @@ CREATE SCHEMA external_stage_schema;
 
 CREATE OR REPLACE STAGE redfin_database_1.external_stage_schema.redfin_ext_stage_yml
 url="s3://sundar28/redfin-transform-zone-yml/redfin_data.parquet/"
-credentials=(aws_key_id='xxxxxxxx'
+credentials=(aws_key_id='AKIA2UC3A6Y7W25YVKEG'
 aws_secret_key='xxxxxx')
 FILE_FORMAT = redfin_database_1.file_format_schema.format_parquet;
 
