de26f11fe441bac65ec923d5a3de9bb9a4c66605
Sundar M  Das <34454614+sundarmd@users.noreply.github.com>
2024-07-19 09:06:36 +0200

    Update real_estate_snowpipe.sql

--- real_estate_snowpipe.sql
+++ real_estate_snowpipe.sql
@@ -1,14 +1,14 @@
-DROP DATABASE IF EXISTS redfin_database_1;
+DROP DATABASE IF EXISTS redfin_database;
 
-CREATE DATABASE redfin_database_1;
+CREATE DATABASE redfin_database;
 
 -- CREATE WAREHOUSE redfin_warehouse;
 
 CREATE SCHEMA redfin_schema;
 
-TRUNCATE TABLE redfin_database_1.redfin_schema.redfin_table;
+TRUNCATE TABLE redfin_database.redfin_schema.redfin_table;
 
-CREATE OR REPLACE TABLE redfin_database_1.redfin_schema.redfin_table (
+CREATE OR REPLACE TABLE redfin_database.redfin_schema.redfin_table (
 period_duration INT,
 city STRING,
 state STRING,
@@ -25,9 +25,9 @@ period_end_month STRING
 );
 
 SELECT *
-FROM redfin_database_1.redfin_schema.redfin_table LIMIT 50;
+FROM redfin_database.redfin_schema.redfin_table LIMIT 50;
 
-SELECT COUNT(*) FROM redfin_database_1.redfin_schema.redfin_table;
+SELECT COUNT(*) FROM redfin_database.redfin_schema.redfin_table;
 
 -- DESC TABLE redfin_database.redfin_schema.redfin_table;
 
@@ -35,7 +35,7 @@ SELECT COUNT(*) FROM redfin_database_1.redfin_schema.redfin_table;
   
 CREATE SCHEMA file_format_schema;
 
-CREATE OR REPLACE file format redfin_database_1.file_format_schema.format_parquet
+CREATE OR REPLACE file format redfin_database.file_format_schema.format_parquet
 type = 'PARQUET'
 compression = 'SNAPPY';
 
@@ -45,31 +45,31 @@ CREATE SCHEMA external_stage_schema;
 
 -- Create staging
 
--- DROP STAGE redfin_database_1.external_stage_schema.redfin_ext_stage_yml;
+-- DROP STAGE redfin_database.external_stage_schema.redfin_ext_stage_yml;
 
-CREATE OR REPLACE STAGE redfin_database_1.external_stage_schema.redfin_ext_stage_yml
+CREATE OR REPLACE STAGE redfin_database.external_stage_schema.redfin_ext_stage_yml
 url="s3://sundar28/redfin-transform-zone-yml/redfin_data.parquet/"
 credentials=(aws_key_id='AKIA2UC3A6Y7W25YVKEG'
 aws_secret_key='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')
-FILE_FORMAT = redfin_database_1.file_format_schema.format_parquet;
+FILE_FORMAT = redfin_database.file_format_schema.format_parquet;
 
-list @redfin_database_1.external_stage_schema.redfin_ext_stage_yml;
+list @redfin_database.external_stage_schema.redfin_ext_stage_yml;
 
 -- Create schema for snowpipe
 
--- DROP SCHEMA redfin_database_1.snowpipe_schema;
+-- DROP SCHEMA redfin_database.snowpipe_schema;
 
-CREATE OR REPLACE SCHEMA redfin_database_1.snowpipe_schema;
+CREATE OR REPLACE SCHEMA redfin_database.snowpipe_schema;
 
 -- Create Pipe
 
-CREATE OR REPLACE PIPE redfin_database_1.snowpipe_schema.redfin_snowpipe
+CREATE OR REPLACE PIPE redfin_database.snowpipe_schema.redfin_snowpipe
 auto_ingest = TRUE
 AS
-COPY INTO redfin_database_1.redfin_schema.redfin_table
-FROM @redfin_database_1.external_stage_schema.redfin_ext_stage_yml
+COPY INTO redfin_database.redfin_schema.redfin_table
+FROM @redfin_database.external_stage_schema.redfin_ext_stage_yml
 FILE_FORMAT = (TYPE = 'PARQUET')
 MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
 
 
-DESC PIPE redfin_database_1.snowpipe_schema.redfin_snowpipe;
+DESC PIPE redfin_database.snowpipe_schema.redfin_snowpipe;
